38a10562a29185158d6d9e3b5528058feb672987
raef <raef.oueslati1996@gmail.com>
2022-03-13 00:24:11 +0100

    Implement the MVVM pattern

---
+++ UltimateTicTacToe.App/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace UltimateTicTacToe.App.ViewModels
+{
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ UltimateTicTacToe.App/ViewModels/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace UltimateTicTacToe.App.ViewModels
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            if (_canExecute == null) return false;
+            return _canExecute();
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute();
+        }
+    }
+}
