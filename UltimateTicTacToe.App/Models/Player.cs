724482db40c96e53dde223c3a634032e131f7243
raef <raef.oueslati1996@gmail.com>
2022-03-13 12:27:31 +0100

    Add player model

---
+++ UltimateTicTacToe.App/Models/Player.cs
@@ -0,0 +1,27 @@
+﻿namespace UltimateTicTacToe.App.Models
+{
+    public enum NatureOfPlayer
+    {
+        Human,
+        Bot
+    }
+
+    public enum BotStrengthLevel
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+    internal class Player
+    {
+        public Player()
+        {
+            PlayerNature = NatureOfPlayer.Human;
+            StrengthLevel = BotStrengthLevel.Easy;
+        }
+
+        public NatureOfPlayer PlayerNature { get; set; }
+        public BotStrengthLevel StrengthLevel { get; set; }
+
+    }
+}
